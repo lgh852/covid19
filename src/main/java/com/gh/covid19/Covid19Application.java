1492e58c3fbd7f822436344df465e3cc84792ee5
lee_gh <koi.ghlee@gmail.com>
2020-07-31 11:24:21 +0900

    방식 변경

--- src/main/java/com/gh/covid19/Covid19Application.java
+++ src/main/java/com/gh/covid19/Covid19Application.java
@@ -2,12 +2,21 @@ package com.gh.covid19;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Covid19Application {
+public class Covid19Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Covid19Application.class, args);
-	}
+//	public static void main(String[] args) {
+//		SpringApplication.run(Covid19Application.class, args);
+//	}
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Covid19Application.class);
+    }
+	public static void main(String[] args) throws Exception {
+	        SpringApplication.run(Covid19Application.class, args);
+	    }
 }
